ca0576f0ee4bc6af2d6c16547d30ee807e60fa80
alvarezantonio <alvarezantoniomail@gmail.com>
2015-04-12 11:50:48 -0500

    requiredValidation file that I forgot to commit last time. You need this for the validation implemented to work. Still in progress.

---
+++ WpfApplication1/RequiredValidationRule.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Globalization;
+
+namespace AfterCareApplication
+{
+    class RequiredValidationRule : ValidationRule
+    {
+
+        public string _propertyName { get; set; }
+
+        public RequiredValidationRule()
+        {
+
+        }
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            AfterCareDataContext db = new AfterCareDataContext();
+            var str = (string)value;
+            if (str == null || str.Length < 1)
+            {
+                return new ValidationResult(false, "*Required field");
+            }
+            else if (_propertyName != null && _propertyName.Length > 1)
+            {
+                switch (_propertyName)
+                {
+                    case "userId":
+                        try
+                        {
+                            List<User> userList = (
+                                from u in db.Users
+                                select u).ToList();
+                            User userExists = (
+                                from u in db.Users
+                                where u.userId.Equals(str)
+                                select u).Single();
+                            int idx = userList.IndexOf(userExists);
+                            if (idx >= 0)
+                            {
+                                return new ValidationResult(false, "*UserID Exists");
+                            }
+                        }
+                        catch (Exception e) {}
+                        break;
+                }
+            }
+            return ValidationResult.ValidResult;
+        }
+    }
+}
